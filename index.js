40fe63db118f0f33cf4596e86d1fde64e7c15ece
nguyendinhtu2002 <nguyendinhtu11022002@gmail.com>
2023-05-06 10:21:29 +0700

    update

--- index.js
+++ index.js
@@ -2,68 +2,92 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const url = require('url');
-const  axios = require('axios');
+const axios = require('axios');
+const { Cluster } = require('puppeteer-cluster');
 
 puppeteer.use(StealthPlugin());
 
 
-// const extractMmsiFromUrl = (url) => {
-//     const regex = /mmsi:(\d+)/;
-//     const matches = url.match(regex);
+const extractMmsiFromUrl = (url) => {
+    const regex = /mmsi:(\d+)/;
+    const matches = url.match(regex);
 
-//     if (matches && matches.length > 1) {
-//         return matches[1];
-//     }
+    if (matches && matches.length > 1) {
+        return matches[1];
+    }
 
-//     return null;
+    return null;
+}
+(async () => {
+    try {
+        const data = fs.readFileSync('filtered_data.json');
+        const jsonData = JSON.parse(data);
+
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: 5,
+            puppeteerOptions: {
+                headless: true,
+            },
+        });
+
+        await cluster.task(async ({ page, data: url }) => {
+            // Delete all cookies
+            const client = await page.target().createCDPSession();
+            await client.send('Network.clearBrowserCookies');
+
+            // Disable cache
+            await page.setCacheEnabled(false);
+
+            await page.goto(url);
+            const currentUrl = page.url();
+            const mmsi = extractMmsiFromUrl(currentUrl);
+            if (mmsi !== null) {
+                console.log(`MMSI for ${url}: ${mmsi}`);
+            }
+        });
+
+        for (const jsonDataItem of jsonData) {
+            const shipId = jsonDataItem.SHIP_ID;
+            const url = `https://www.marinetraffic.com/en/ais/details/ships/shipid:${shipId}`;
+            await cluster.queue(url);
+        }
+
+        await cluster.idle();
+        await cluster.close();
+
+    } catch (error) {
+        console.log(error)
+    }
+})();
+
+// Số mini giây ban đầu
+
+// Hàm tính toán số mini giây sau mỗi 2 phút
+// function calculateMs() {
+//   ms += 2 * 60 * 1000;  // Chuyển đổi 2 phút sang mini giây và cộng vào số mini giây hiện tại
+//   ms += 1683333693;     // Cộng thêm 1683333693 mini giây
+//   return ms;
 // }
-// // const data = require("./data.json")
+
+
 // (async () => {
-//     const tempData = ["255806028","311038200","257960000"]
-//     try {
-//         const data = fs.readFileSync('filtered_data.json');
-//         const jsonData = JSON.parse(data);
-//         const browser = await puppeteer.launch({
+//     const browser = await puppeteer.launch({});
+//     const page = await browser.newPage();
 
-//         });
-//         const page = await browser.newPage();
-//         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+//     // Enable request interception
+//     await page.setRequestInterception(true);
 
 
-//         for (let i = 0; i < jsonData.length; i++) {
+//     await page.goto('https://www.marinetraffic.com/en/ais/home/centerx:115.916/centery:20.354/zoom:10');
 
-//             // Delete all cookies
-//             const client = await page.target().createCDPSession();
-//             await client.send('Network.clearBrowserCookies');
+//     // Wait for all requests to finish
+//     await page.waitForRequest(() => true);
 
-//             // Disable cache
-//             await page.setCacheEnabled(false);
-
-//             const shipId = jsonData[i].SHIP_ID;
-//             const url = `https://www.marinetraffic.com/en/ais/details/ships/shipid:${shipId}`;
-//             await page.goto(url);
-//             const currentUrl = page.url();
-//             const mmsi = extractMmsiFromUrl(currentUrl);
-//             if (mmsi !== null) {
-//                 jsonData[i].MMSI = mmsi;
-//                 console.log("Thanh cong thu " + i);
-//             }
-//             else {
-//                 console.log("That bau thu " + i);
-
-//             }
-//         }
-
-//         fs.writeFileSync('updated_data.json', JSON.stringify(jsonData));
-
-//         await browser.close();
-
-//     } catch (error) {
-//         console.log(error)
-//     }
+//     await browser.close();
 // })();
-
-
+// In ra số mini giây sau mỗi 2 phút
+// console.log(calculateMs());
 
 
 
